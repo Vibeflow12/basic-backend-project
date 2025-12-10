40b12167dbcc9b9831f23d6ed5474832b854f1a2
prashanth <kasamsaimaniprashanth@gmail.com>
2025-12-10 22:26:02 +0530

    user api  is created

--- src/app.js
+++ src/app.js
@@ -2,4 +2,9 @@ import express from "express"
 
 const app = express();
 
+app.use(express.json());
+
+import userRouter from "./routes/user.route.js"
+
+app.use("/api/v1/users", userRouter)
 export default app;
--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`)
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`mongodb connected ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("mongodb connection failed");
+        console.log("mongodb connection failed", error.message);
         process.exit(1)
     }
 }
