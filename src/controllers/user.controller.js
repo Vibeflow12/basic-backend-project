8ec52f7b9c23ebcccd4eebc9c782853c1e86381b
prashanth <kasamsaimaniprashanth@gmail.com>
2025-12-10 21:10:10 +0530

    user route is created and user controller is created

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,38 @@
+import { User } from "../models/user.model";
+
+const registerUser = async (req, res) => {
+    try {
+        const { username, email, password } = req.body
+
+        //basic validation
+
+        if (!username || !email || !password) {
+            return res.status(400).json({ message: "all fields are empty" });
+
+        }
+
+        // to check if user exists
+        const existing = await User.findOne({ email: email.toLowerCase() })
+        if (existing) {
+            return res.status(400).json({ message: "user already exists" });
+        }
+
+        //create user
+        const user = await User.create({
+            username,
+            email: email.toLowerCase(),
+            password,
+        });
+
+        res.status(201).json({
+            message: "User registered",
+            user: { id: user._id, email: user.email, username: user.username }
+        })
+
+    } catch (error) {
+        res.status(500).json({ message: "internal server error", error: error.message })
+
+    }
+}
+
+export { registerUser }
